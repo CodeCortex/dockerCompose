d810e25225f3ef04253c1f5dd182924b2d49cf6b
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2026-09-04 15:12:34 +0530

    Add Kong service with custom authentication plugin and configuration

---
+++ kong/plugins/custom-auth/handler.lua
@@ -0,0 +1,92 @@
+local http = require "resty.http"
+
+local CustomAuthHandler = {
+  PRIORITY = 1000,
+  VERSION = "1.0.0",
+}
+
+function CustomAuthHandler:access(config)
+
+  -- Get Authorization header from incoming request
+  local authorization = kong.request.get_header("Authorization")
+
+  if not authorization then
+    return kong.response.exit(401, {
+      message = "Authorization header is required"
+    })
+  end
+
+  -- Create HTTP client
+  local httpc = http.new()
+
+  httpc:set_timeouts(
+    10000,
+    10000,
+    10000
+  )
+
+  -- Call auth service
+  local res, err = httpc:request_uri(
+    config.auth_service_url,
+    {
+      method = "GET",
+
+      headers = {
+        ["Authorization"] = authorization
+      }
+    }
+  )
+
+  -- Auth service unavailable
+  if not res then
+
+    kong.log.err(
+      "Failed to call auth service: ",
+      err
+    )
+
+    return kong.response.exit(
+      500,
+      {
+        message = "Authentication service unavailable"
+      }
+    )
+  end
+
+  -- Token invalid
+  if res.status ~= 200 then
+
+    return kong.response.exit(
+      401,
+      {
+        message = "Unauthorized"
+      }
+    )
+  end
+
+  -- Auth service should return user ID
+  local user_id = res.body
+
+  if not user_id or user_id == "" then
+
+    kong.log.err(
+      "Auth service returned empty user ID"
+    )
+
+    return kong.response.exit(
+      401,
+      {
+        message = "Invalid authentication response"
+      }
+    )
+  end
+
+  -- Add user ID to request sent to upstream service
+  kong.service.request.set_header(
+    "X-User-ID",
+    user_id
+  )
+
+end
+
+return CustomAuthHandler
---
+++ kong/plugins/custom-auth/schema.lua
@@ -0,0 +1,31 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+return {
+  name = "custom-auth",
+
+  fields = {
+    {
+      consumer = typedefs.no_consumer
+    },
+
+    {
+      protocols = typedefs.protocols_http
+    },
+
+    {
+      config = {
+        type = "record",
+
+        fields = {
+          {
+            auth_service_url = {
+              type = "string",
+              required = true,
+              default = "http://authService:9898/auth/ping"
+            }
+          }
+        }
+      }
+    }
+  }
+}
